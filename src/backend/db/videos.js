ad3f5794cabb5b8221a501009f8f9b9d6854d658
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-03-22 15:40:54 +0530

    added JSON data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -2,20 +2,105 @@
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
  * */
+import { v4 as uuid } from "uuid";
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: uuid(),
+    video_id: "ar31DrNV_pM",
+    title: "Designing a Car – from Sketch to Presentation",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1spLQQeuxH-Rfugz3cSCv2qJGC5sfjwHD",
+    categoryName: "Design",
+    views: 0,
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Interested in knowing how a car is designed? Here's an older video from the Audi design archives showing the Sport Quattro concept development from the first sketches to the official presentation.",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    video_id: "vuEMhfD0eCI",
+    title: "Lamborghini Huracán design concept",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1spLQQeuxH-Rfugz3cSCv2qJGC5sfjwHD",
+    categoryName: "Design",
+    views: 0,
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Interview with Filippo Perini, Head of Design at Automobili Lamborghini, about the LB 724 concept a.k.a. Lamborghini Huracán: a car with a clear and extreme design but at the same time daily usable. ",
+  },
+  {
+    _id: uuid(),
+    video_id: "Xi4RrjvZA7w",
+    title: "Designing the future: Lamborghini Terzo Millennio",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1spLQQeuxH-Rfugz3cSCv2qJGC5sfjwHD",
+    categoryName: "Design",
+    views: 0,
+    description:
+      "Unveiled at EmTech 2017, in partnership with MIT, the Terzo Millennio concept car is conceived to include groundbreaking energy storage systems, innovative materials, breathtaking propulsion and design.",
+  },
+  {
+    _id: uuid(),
+    video_id: "fCrXVS2Q9A8",
+    title: "Lamborghini Aventador SVJ Engine",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1QLNJx12jzH2Lt8h9qIGzTf2l5w79rPjH",
+    categoryName: "Engine",
+    views: 0,
+    description:
+      "Let yourself be captured by the 8D sounds of the indomitable naturally aspirated V12 engine with 770 horsepower of Lamborghini Aventador SVJ. Put your headphones on and watch the video to start your experience. ",
+  },
+  {
+    _id: uuid(),
+    video_id: "WUL8xQ0K-qs",
+    title: "Lamborghini Engines: 350 GT and 8 Liter Marine",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1QLNJx12jzH2Lt8h9qIGzTf2l5w79rPjH",
+    categoryName: "Engine",
+    views: 0,
+    description:
+      "Andrew Romanowski of the Lamborghini Club America stops by the garage with Rob Cleary to show Jay the smallest and the largest Lamborghini V12's - the 3.5 liter version used in the 350GT, and the 8.0 liter marine version used in offshore racing.",
+  },
+  {
+    _id: uuid(),
+    video_id: "vEvBbNe3rqU",
+    title: "LAMBORGHINI AVENTADOR LP 700-4. A RELENTLESS FORCE",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1QLNJx12jzH2Lt8h9qIGzTf2l5w79rPjH",
+    categoryName: "Engine",
+    views: 0,
+    description: "Automobili Lamborghini S.p.A.",
+  },
+  {
+    _id: uuid(),
+    video_id: "uttfdWiiUa4",
+    title: "High Five for Mahindra XUV700",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1jNInJ_l79Qs46DR-07VN8F3ZbxttI1J3",
+    categoryName: "Safety",
+    views: 0,
+    description:
+      "Mahindra’s latest SUV achieved five stars for adult occupant and four stars for child occupants in Global NCAP’s latest #SaferCarsForIndia crash tests. ",
+  },
+  {
+    _id: uuid(),
+    video_id: "SSA3Nu6NeTk",
+    title: "Tata Punch achieves 5 star rating for adult protection",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1jNInJ_l79Qs46DR-07VN8F3ZbxttI1J3",
+    categoryName: "Safety",
+    views: 0,
+    description:
+      "The Tata Punch has achieved a five star rating for adult occupant protection and four stars for child occupant protection in Global NCAP’s latest #SaferCarsForIndia crash tests. ",
+  },
+  {
+    _id: uuid(),
+    video_id: "omW1q05nUak",
+    title: "Four star Thar",
+    display_img:
+      "https://drive.google.com/uc?export=view&id=1jNInJ_l79Qs46DR-07VN8F3ZbxttI1J3",
+    categoryName: "Safety",
+    views: 0,
+    description:
+      "The Mahindra Thar has achieved an encouraging four star rating for both adult and child occupants in Global NCAP’s new round of #SaferCarsForIndia crash tests. ",
   },
 ];
